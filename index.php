6e8451e1ac11d520988af6d2a93419a56c31416f
rismadewi12 <79685865+rismadewi12@users.noreply.github.com>
2021-02-26 15:14:12 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>YeppaCosmetic</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="" />
+    <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+    <link rel="canonical" href="" />
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="" />
+    <meta property="og:description" content="" />
+    <meta property="og:url" content="" />
+    <meta property="og:site_name" content="" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </style>
+</head> 
+
+<body>
+<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="YeppaCosmetic">Product</a>
+			</div>
+			<ul class="nav navbar-nav">
+        <li><a href="#" style>SkinCare</a></li>
+				<li><a href="#">HairCare</a></li>
+				<li><a href="#">BodyCare</a></li> 
+				<li><a href="#">MakeUp</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="#"><span class="glyphicon glyphicon-user"></span> Contact</a></li>
+			<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> AboutUs</a></li>
+			</ul>
+		</div>
+	</nav>
+  <div class="jumbotron text-center" style="background-image: url(asset/129.jpg);" style>
+  <h1>YEPPA COSMETIC</h1>
+  <p>Selamat datang,Happy Shopping Unnie</p> 
+</div>
+<section class="services">
+                <div class="service">
+                    <div class="icon"><img src="asset/31.jpg" width="70%"></div>
+                </div>
+                <div class="service">
+                    <div class="icon"><img src="asset/34.jpg" width="70%"></div> 
+                </div>
+                <div class="service">
+                    <div class="icon"><img src="asset/po.jpg" width="70%"></div>    
+                </div>
+            </section>
+  </div>
+</div>
+
+</body>
+</html>
